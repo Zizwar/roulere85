d057e5eefd3894a2b6fb69d7b546882615485d92
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2022-11-12 01:37:15 +0100

    Update and rename data.json to data.ts

---
+++ utils/data.ts
@@ -0,0 +1,79 @@
+export default {
+  "url": "https://roulere85.com/",
+  "title": "Roulez E85",
+  "description": "Payer moins cher votre carburant",
+  "mail": "contact@roulere85.com",
+
+  "contents": [
+    {
+      "count": "01",
+      "title": "pour les 200 premiers inscrits",
+      "subTitle": "Promotions",
+      "text": `On vous rembourse le premier plein
+Parrainez Quelqu'un On vous offre le plein !!!
+Roulez intelligent, Roulez E85  
+Roulez safe et nature et ne changez pas de voiture, changez d’habitudes et faites des économies tout en participant à la protection de notre environnement et à préparer un monde propre pour nos successeurs   
+Aidez à réduire votre empreinte carbone, Réduisez de 3/4 votre émission de CO2 en passant de  L'essence au bioéthanol  ;
+Bon à savoir ;
+85% Bio 15% Pétrole ;
+Payer moins cher votre carburant ;
+prix moyen essence : 2.10€ ;
+prix moyen bioethanol : 0.81€ ;
+Kit bioéthanol rentabilisé en moyenne en 1 an (6 mois avec la subvention accordée PACA Subventionne à hauteur de 500€ par la région PACA ;
+Pour en savoir plus Rendez-vous sur https://www.maregionsud.fr/toutes-vos-aides/detail/le-cheque-transition-bioethanol;
+`,
+      "link": "https://www.maregionsud.fr/toutes-vos-aides/detail/le-cheque-transition-bioethanol",
+      "image": "images/01.png"
+    },
+  {
+      "count": "02",
+      "title": "Qu’est-ce que c’est ? ",
+      "subTitle": "Roulez intelligent",
+      "text": `
+Le bioéthanol-E85 est un biocarburant. Un biocarburant est un carburant liquide issu de la transformation de matières végétales produites par l’agriculture comme les céréales (le blé, le maïs) ou encore la betterave à sucre, etc. Il existe deux filières de production des biocarburants : ;
+La filière esters : mélangés à du gazole, les esters (aussi appelés « biodiesel ») sont obtenus suite à une réaction entre une huile végétale (colza ou soja) et du méthanol ; 
+La filière bioéthanol : incorporé dans les supercarburants, le sucre contenu dans les matières premières utilisées pour concevoir le bioéthanol est transformé en alcool par fermentation et dégage alors du gaz carbonique (CO2). ;
+
+Des économies de carburant car le bioéthanol est disponible à la pompe à un prix très compétitif ; 
+Une consommation plus écologique grâce à un carburant plus respectueux de l’environnement ; 
+Vous participez à faire vivre l’agriculture en France puisque la majorité des biocarburants en sont issus ; 
+`,
+      "link": "https://www.maregionsud.fr/toutes-vos-aides/detail/le-cheque-transition-bioethanol",
+      "image": "images/02.png"
+    },
+,
+  {
+      "count": "03",
+      "title": "Voici les avantages que vous pouvez en retirer",
+      "subTitle": "spécifiques au niveau régional ou départemental",
+      "text": `
+Vous pouvez bénéficier de certaines subventions spécifiques au niveau régional ou départemental ; 
+Le prix du certificat d’immatriculation (ex-carte grise) peut être minoré, voire totalement gratuit, selon votre zone géographique ; 
+Une facilité d’implémentation : homologuer votre voiture essence (si cette dernière a été construite après les années 2000) au bioéthanol est relativement facile ; 
+Le bioéthanol améliore les performances de votre moteur : le bioéthanol contient un indice d’octane (qui mesure la résistance à l’auto-allumage) supérieur aux SP95 et SP98. Cela améliore la combustion et augmente alors la performance de votre moteur ; 
+Sur un plan plus sociétal, le bioéthanol semble plus bénéfique pour l’économie française puisqu’étant produit essentiellement en France, c’est le gouvernement français qui contrôle les quantités fabriquées, la qualité du carburant produit ainsi que son coût de fabrication et son prix de vente. ;
+Comment fonctionne un boîtier E85 ? ;
+Lorsque vous faites homologuer votre voiture à l’utilisation du bioéthanol, concrètement le professionnel agréé installe un boîtier spécifique qui permet au moteur d’injecter la bonne quantité de carburant en fonction de la proportion d’éthanol présente dans le réservoir pour optimiser son fonctionnement.; 
+Le concept est simple : une sonde mesure la proportion d’éthanol dans le carburant, plus la proportion est élevée, plus le boîtier augmente la quantité de carburant qu’il injecte. De cette façon, le moteur reçoit la bonne quantité d’énergie. ;
+`,
+      "link": "https://www.maregionsud.fr/toutes-vos-aides/detail/le-cheque-transition-bioethanol",
+      "image": "images/01.png"
+    },
+    {
+      "count": "04",
+      "title": "Agenda!",
+      "subTitle": "",
+      "text": " Determining what level of hiker you are can be an important tool when planning future hikes. This hiking level guide will help you plan hikes according to different hike ratings set by various websites like All Trails and Modern Hiker. What type",
+      "link": "https://www.google.com?q=RoulezE85",
+      "types": "agenda"
+    },
+    {
+      "count": "05",
+      "title": "Adresse",
+      "subTitle": "M. Obert Vincent",
+      "text": "19 Imp. de la Gaieté \n 19 Imp. de la Gaieté, 06200 Nice, France. \n mail: contact@roulere85.com \n tel: 060188688012",
+      "link": "https://www.google.com?q=RoulezE85",
+      "types": "maps"
+    }
+  ]
+}
